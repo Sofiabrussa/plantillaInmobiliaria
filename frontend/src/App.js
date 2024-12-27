57129be6524c91b88a52470c31261420d3b13e5e
Sofia Brussa <sofiabrussa@gmail.com>
2024-12-27 11:40:06 -0300

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
-import Home from './components/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Filtro from './components/Filtro/Filtro';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-    </div>
-  );
-}
+  const PropiedadesPage = () => {
+    return (
+      <div>
+        <Navbar/>
+        <Filtro /> {/* Mostrar el componente Filtro */}
+      </div>
+    );
+  };
+
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* Ruta para /api/propiedades */}
+            <Route path="/api/propiedades" element={<PropiedadesPage />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  };
+  
 
 export default App;
---
+++ frontend/src/components/Filtro/Filtro.jsx
@@ -0,0 +1,13 @@
+// Filtro.js
+import React from 'react';
+
+const Filtro = () => {
+  return (
+    <div>
+      <h2>Filtro de Propiedades</h2>
+      {/* Aquí puedes agregar los campos de filtro, como selectores o input */}
+    </div>
+  );
+};
+
+export default Filtro;
